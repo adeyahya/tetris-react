60e31fd5a536a1acae208f04ab02d24f4bbbd913
Ade Yahya <adeyahyaprasetyo@gmail.com>
2020-02-20 00:22:27 +0700

    create tetris class

---
+++ src/Tetris.ts
@@ -0,0 +1,59 @@
+interface TetrisArgs {
+  canvas: HTMLCanvasElement;
+  squareSize?: number;
+  strokeColor?: string;
+  boardColor?: string;
+  rowSize?: number;
+  columnSize?: number;
+}
+
+interface TetrisInterface {
+  drawBoard(): void;
+  drawSquare(x: number, y: number, color?: string): void;
+}
+
+class Tetris implements TetrisInterface {
+  ctx: CanvasRenderingContext2D;
+  squareSize: number;
+  strokeColor: string;
+  boardColor: string;
+  rowSize: number;
+  columnSize: number;
+  board: string[][];
+  constructor(args: TetrisArgs) {
+    this.ctx = args.canvas.getContext('2d');
+    this.squareSize = args.squareSize || 20;
+    this.strokeColor = args.strokeColor || 'black';
+    this.boardColor = args.boardColor || 'white';
+    this.rowSize = args.rowSize || 20;
+    this.columnSize = args.columnSize || 10;
+    this.board = (() => {
+      let board = [];
+      for (let row = 0; row < this.rowSize; row++) {
+        board[row] = [];
+        for(let column = 0; column < this.columnSize; column++){
+          board[row][column] = this.boardColor;
+        }
+      }
+      return board;
+    })();
+  }
+
+  drawBoard() {
+    this.board.forEach((row, rowIndex) => {
+      for (let columnIndex = 0; columnIndex < row.length; columnIndex++) {
+        this.drawSquare(columnIndex, rowIndex);
+      }
+    })
+  }
+
+  drawSquare(x, y, color = this.boardColor) {
+    this.ctx.fillStyle = color;
+    this.ctx.fillRect(x*this.squareSize, y*this.squareSize, this.squareSize, this.squareSize);
+
+    this.ctx.strokeStyle = "BLACK";
+    this.ctx.strokeRect(x*this.squareSize,y*this.squareSize,this.squareSize,this.squareSize);
+  }
+}
+
+export default Tetris;
--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,48 +1,30 @@
 import React, {FunctionComponent, useRef, useEffect} from 'react';
-
-const SQ = 40;
-const ROW = 20;
-const COL = 10;
-// a VACANT (empty) square has a white (#FFF) color
-const VACANT = '#FFF';
-
-// now we define the board array.
-let board: String[][] = [];
-
-for ( let r = 0; r < ROW; r++){
-  board[r] = [];
-  // let's create the columns
-  for( let c = 0; c < COL; c++){
-    board[r][c] = VACANT;
-    // when we first draw the board all the square are empty, so every square has the value "#FFF".
-  }
-}
+import Tetris from '../Tetris';
 
 const Board: FunctionComponent = () => {
-  const canvas = useRef(null);
-
-  const drawBoard = (ctx) => {
-    board.forEach((item, i) => {
-      item.forEach((child, j) => {
-        ctx.fillStyle = child;
-        ctx.strokeRect(j * SQ, i * SQ, SQ, SQ);
-        ctx.fillRect(j * SQ, i * SQ, SQ, SQ);
-      })
-    })
-  }
+  const canvas: React.MutableRefObject<HTMLCanvasElement> = useRef();
+  const tetris: React.MutableRefObject<Tetris> = useRef();
 
   useEffect(() => {
-    if (canvas.current !== null) {
-      const ctx = canvas.current.getContext('2d');
-      ctx.strokeStyle = "black";
-      drawBoard(ctx);
+    if (canvas.current) {
+      tetris.current = new Tetris({
+        canvas: canvas.current,
+        squareSize: 40,
+        rowSize: 20,
+        columnSize: 10,
+      });
+
+      tetris.current.drawBoard();
     }
   }, []);
 
   return (
-    <canvas style={{
-      border: 'solid black 4px'
-    }} ref={canvas} width={400} height={800} />
+    <>
+      <canvas style={{
+        border: 'solid black 2px'
+      }} ref={canvas} width={400} height={800} />
+      <button>draw</button>
+    </>
   )
 }
 
